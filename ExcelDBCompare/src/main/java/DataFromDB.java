28aef2e34b9baedb9b45acc19f484386b7df5f1d
anuragsingh <anurag2apr@gmail.com>
2023-02-23 00:00:30 +0530

    ExcelDB Utility

---
+++ ExcelDBCompare/src/main/java/DataFromDB.java
@@ -0,0 +1,42 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DataFromDB {
+
+	public DataFromDB() {
+		dataFromDb();
+	}
+
+	public static void main(String[] args) {
+
+	}
+	
+	public List<InputData> dataFromDb(){
+
+		MysqlConnect mysqlConnect = new MysqlConnect();
+		List<InputData> lstInputData = new LinkedList<InputData>();
+		String sql = "SELECT * FROM `details`";
+		try {
+			PreparedStatement statement = mysqlConnect.connect().prepareStatement(sql);
+			ResultSet rs = statement.executeQuery(sql);
+
+			while(rs.next()) {
+				InputData inputData = new InputData();
+				inputData.setId(String.valueOf(rs.getInt("id")));
+				inputData.setName(String.valueOf(rs.getString("name")));
+				inputData.setPinCode(String.valueOf(rs.getString("pincode")));
+				inputData.setGender(String.valueOf(rs.getString("gender")));
+				inputData.setCity(String.valueOf(rs.getString("city")));
+				lstInputData.add(inputData);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			mysqlConnect.disconnect();
+		}
+		return lstInputData;
+	}
+}
---
+++ ExcelDBCompare/src/main/java/DataFromExcel.java
@@ -0,0 +1,54 @@
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class DataFromExcel {
+
+	public DataFromExcel() {
+		//readExcelFile(CompareDataMain.FILE_PATH);
+	}
+
+	public static void main(String[] args) {
+
+	}
+	
+	public  List<InputData> readExcelFile(String file) {
+		boolean firstLine =true;
+		Row nextRow = null;
+		List<InputData> lstInputData = new LinkedList<InputData>();
+		try(Workbook wb = new XSSFWorkbook(new File(file))){
+			
+			Sheet firstSheet = wb.getSheetAt(0);
+			Iterator<Row> itr = firstSheet.rowIterator();
+			int noOfColumns = firstSheet.getRow(0).getPhysicalNumberOfCells();
+			System.out.println("No of columns "+noOfColumns);
+			while(itr.hasNext()) {
+				nextRow = itr.next();
+				if(firstLine) {
+					firstLine = false;
+					continue;
+				}
+				InputData inputData = new InputData();
+				inputData.setId(String.valueOf(nextRow.getCell(0)).replaceAll("[0]+$", "").replaceAll("(\\.)(?!.*?[1-9]+)", "").trim());
+				inputData.setName(String.valueOf(nextRow.getCell(1)).trim());
+				inputData.setPinCode(String.valueOf(nextRow.getCell(2)).replaceAll("[0]+$", "").replaceAll("(\\.)(?!.*?[1-9]+)", "").trim());
+				inputData.setGender(String.valueOf(nextRow.getCell(3)).trim());
+				inputData.setCity(String.valueOf(nextRow.getCell(4)).trim());
+				lstInputData.add(inputData);
+			}
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return lstInputData;
+	}
+	
+}
